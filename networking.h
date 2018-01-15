8b4485bd06c0064b6493a00b099c6fbee0430f4f
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2018-01-15 17:42:39 -0500

    lol

--- networking.h
+++ networking.h
@@ -1,11 +1,12 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 
-#define MESSAGE_BUFFER_SIZE 1000
+#define BUFFER_SIZE 1000
 
 void error_check( int i, char *s );
 
 int server_setup();
+int client_setup(char *);
 int server_connect(int sd);
 
 int client_connect( char *host );
@@ -16,3 +17,14 @@ int receive_data(int connection_descript,void *data);
 
 
 #endif
+
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <sys/socket.h>
