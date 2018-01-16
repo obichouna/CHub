a8b0855d128fc436b5689df3cad3607346b0b550
dariashifrina <dshifrina@stuy.edu>
2018-01-15 19:08:19 -0500

    network stuffs

--- client.c
+++ client.c
@@ -1,7 +1,23 @@
 #include "networking.h"
 
-int main(int argc, char **argv) {
 
+//creating repo
+
+char * ask_for_dir(){
+  printf("Please type the directory name you would like to use and press enter: ");
+  char * buffer = calloc(1024,sizeof(char));
+  fgets(buffer, 1024, stdin);
+  char * newline = strchr(buffer,'\n');
+  *newline = 0;
+  return buffer;
+}
+
+int create_dir(char *dir_name) {
+  int status = 0;
+  return status;
+} 
+
+int main(int argc, char **argv) {
   int server_socket;
   char buffer[BUFFER_SIZE];
 
@@ -10,12 +26,22 @@ int main(int argc, char **argv) {
   else
     server_socket = client_setup( TEST_IP );
 
+  // while (1) {
+    /* char * dir_name = ask_for_dir();
+    // *strchr(dir_name, '\n') = 0;
+    write(server_socket, buffer, sizeof(buffer));
+    read(server_socket, buffer, sizeof(buffer));
+    printf("received: [%s]\n", buffer);
+    // create_dir(ask_for_dir());
+    //printf("enter directory name: ");
+    }*/
+
   while (1) {
-    printf("enter data: ");
+    printf("enter directory name: ");
     fgets(buffer, sizeof(buffer), stdin);
     *strchr(buffer, '\n') = 0;
     write(server_socket, buffer, sizeof(buffer));
     read(server_socket, buffer, sizeof(buffer));
     printf("received: [%s]\n", buffer);
-  }
+    }
 }
