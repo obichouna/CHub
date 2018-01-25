d3a355253ba210e43ad9c9f299950306269d1418
Dasha <dshifrina@stuy.edu>
2018-01-25 09:45:13 -0500

    fdsfsd

--- select_client.c
+++ select_client.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
   fd_set read_fds;
 
   if (argc == 2)
-    server_socket = client_setup( TEST_IP);
+    server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( buffer0);
 
