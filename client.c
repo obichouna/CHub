ef06b519db198a01ac2842f8e43d7d71c9b5003d
dariashifrina <dshifrina@stuy.edu>
2018-01-24 21:35:41 -0500

    dasha

--- client.c
+++ client.c
@@ -48,17 +48,6 @@ int main(int argc, char **argv) {
     server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( TEST_IP );
-
-  // while (1) {
-    /* char * dir_name = ask_for_dir();
-    // *strchr(dir_name, '\n') = 0;
-    write(server_socket, buffer, sizeof(buffer));
-    read(server_socket, buffer, sizeof(buffer));
-    printf("received: [%s]\n", buffer);
-    // create_dir(ask_for_dir());
-    //printf("enter directory name: ");
-    }*/
-
   while (1) {
     printf("enter directory name: ");
     fgets(buffer, sizeof(buffer), stdin);
